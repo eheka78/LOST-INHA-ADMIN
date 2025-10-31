9344747726be8e20f2bf774f810c3146d897ea41
eheka78 <gemddkim22@gmail.com>
2025-11-01 08:41:04 +0900

    feat: 로고 클릭 -> home으로

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 
 export default function Logo() {
     return (
-        <div className={styles.Logo_Container}>
-            <img
-                src="./images/INHA_logo.png"
-                style={{ height:"50px" }}
-            />
+        <Link to='/'>
+            <div className={styles.Logo_Container}>
+                <img
+                    src="./images/INHA_logo.png"
+                    style={{ height:"50px" }}
+                />
 
-            <div>
-                <div style={{ fontFamily: "Pretendard-SemiBold", fontSize:"20px" }}>
-                    분실물 찾기
+                <div>
+                    <div style={{ fontFamily: "Pretendard-SemiBold", fontSize:"20px" }}>
+                        분실물 찾기
+                    </div>
+                    <div style={{ fontFamily: "Pretendard-Medium", fontSize:"13px" }}>관리자 페이지</div>
                 </div>
-                <div style={{ fontFamily: "Pretendard-Medium", fontSize:"13px" }}>관리자 페이지</div>
             </div>
-        </div>
+        </Link>
     )
 }
