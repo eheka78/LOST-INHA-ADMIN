47abc480e9d0224d59f43da352abce363b774e5b
eheka78 <gemddkim22@gmail.com>
2025-11-07 00:50:30 +0900

    feat: refresh

--- src/api/api.js
+++ src/api/api.js
@@ -1,42 +1,53 @@
-import { TokenStore } from "../TokenStore";
 import axios from "axios";
+import { TokenStore } from './../TokenStore';
+import { profile } from "./auth";
 
-// axios 
 const api = axios.create({
     baseURL: "/api",
     withCredentials: true // 기본값: 쿠키 전송 허용
 });
 
+// 새로고침 시 refresh 실행
+(async () => {
+  try {
+    const refreshRes = await axios.post("/api/auth/refresh", {}, { withCredentials: true });
+    
+    
+    const newAccessToken = refreshRes.data.accessToken;
+    TokenStore.setToken(newAccessToken);
 
-// Access Token 자동 붙이기
+    profile();
+    
+    console.log("🔄 새로고침 시 토큰 갱신 성공");
+  } catch (e) {
+    console.warn("🔄 새로고침 시 토큰 갱신 실패", e);
+    TokenStore.clearToken();
+  }
+})();
+
+// 요청 시 Access Token 붙이기
 api.interceptors.request.use(cfg => {
     // 로그인 요청은 쿠키 전송 안 함
-    if (cfg.url === "/auth/login") {
+    if (cfg.url === "/api/auth/login") {
         cfg.withCredentials = false;
         return cfg;
     }
 
     const token = TokenStore.getToken();
-    // console.log("token: "  + token);
+
     if (token) {
         cfg.headers.Authorization = `Bearer ${token}`;
     }
-    
     return cfg;
 });
 
+let isRefreshing = false; // refresh 한 번만 되게
 
-// refresh 요청 중복 방지 변수
-let isRefreshing = false; 
-
-
-// 응답 인터셉터 (401 처리)
 api.interceptors.response.use(
   res => res,
-  async (error) => {
+  async error => {
     const originalRequest = error.config;
 
-    // 토큰 만료 => 401
     if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
         // 이미 refresh 요청 중이면 그대로 reject해서 무한 반복 방지
@@ -47,7 +58,6 @@ api.interceptors.response.use(
       isRefreshing = true;
 
       try {
-        // refresh 요청
         const refreshRes = await axios.post(
           "/api/auth/refresh",
           {},
@@ -57,15 +67,17 @@ api.interceptors.response.use(
         const newAccessToken = refreshRes.data.accessToken;
         TokenStore.setToken(newAccessToken);
 
+        profile();
+
         // 실패했던 요청에 새 토큰 붙이기
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
         isRefreshing = false;
-        
         return api(originalRequest); // 재요청
       } catch (refreshError) {
         console.error("토큰 재발급 실패", refreshError);
         TokenStore.clearToken();
-        window.location.href = "/Login";
+        window.location.href = "/login";
         isRefreshing = false;
         return Promise.reject(refreshError);
       }
@@ -74,23 +86,6 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-
-// 새로고침 시 refresh 실행
-(async () => {
-  try {
-    const refreshRes = await axios.post("/api/auth/refresh", {}, { withCredentials: true });
-
-    const newAccessToken = refreshRes.data.accessToken;
-    TokenStore.setToken(newAccessToken);
-
-    console.log("새로고침 시 토큰 갱신 성공");
-  } catch (e) {
-    console.warn("새로고침 시 토큰 갱신 실패", e);
-    TokenStore.clearToken();
-  }
-})();
-
 
 
 export default api;
